21f6f42f975ac1e93077146ca9f485098a5460a6
Ariffin <ariffinanuar@gmail.com>
2025-10-05 10:07:49 +0800

    Fix Express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -331,7 +331,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // Serve React app for all other routes (production)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
